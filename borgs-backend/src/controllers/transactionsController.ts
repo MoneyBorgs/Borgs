d8e8c5eee395dd160f2fafa7fc2df15ae8cfd718
Murilo Calegari de Souza <murilo478@gmail.com>
2022-10-16 03:06:50 -0400

    Most recent changes

--- borgs-backend/src/controllers/transactionsController.ts
+++ borgs-backend/src/controllers/transactionsController.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Params, Post, Put, Response } from '@decorators/express';
+import { Controller, Get, Params, Patch, Post, Put, Response } from '@decorators/express';
 import dbPool from '../db/dbPool';
 import { updateTransactionById } from '../util/queryBuilder';
+import format from 'pg-format';
+import { ClientBase, PoolClient } from 'pg';
+import Transaction from '../model/Transaction';
 
 @Controller('/')
 export default class TransactionsController {
@@ -62,54 +65,28 @@ export default class TransactionsController {
 	@Post("/transaction/:userId")
 	async createTransactionForUser(req, res) {
 
+		const t : Transaction = req.body;
+
 		const client = await dbPool.connect()
 		try {
-			const t = req.body;
-			
 			await client.query('BEGIN')
-			const result = await client.query(
-				`
-					INSERT INTO Transactions(
-						virtual_account,
-						physical_account,
-						value,
-						category,
-						timestampEpochSeconds,
-						description,
-						notes
-					) VALUES ($1,$2,$3,$4,$5,$6,$7)
-					RETURNING transaction_id;
-				`,
-				[t.virtual_account, t.physical_account, t.value, t.category, t.timestampEpochSeconds, t.description, t.notes]
-			);
 
-			t.transaction_id = result.rows[0].transaction_id;
+			await this.insertTransaction(client, t);
 
-			for(const tag of t.tags) {
-				await client.query( // TODO: Do within single transaction
-					`
-						INSERT INTO Tags(
-							tag,
-							transaction_id
-						) VALUES ($1,$2);
-					`,
-					[tag, t.transaction_id]
-				);
-			}
 			await client.query('COMMIT')
 			res.send(t);
-		} catch(e) {
+		} catch (e) {
 			await client.query('ROLLBACK')
-			throw(e);
+			throw (e);
 		} finally {
 			client.release();
 		}
 	}
 
-	@Put("/transaction/:userId/:transactionId")
+	@Patch("/transaction/:userId/:transactionId")
 	async updateTransaction(req, res) {
 		const userId = req.params.userId;
-		const transactionId = req.params.transactionId
+		const transactionId: number = req.params.transactionId
 
 		const tags = req.body.tags;
 
@@ -117,7 +94,7 @@ export default class TransactionsController {
 		delete transactionTableParameters.tags;
 
 		var transactionQuery = updateTransactionById(transactionId, transactionTableParameters);
-		
+
 		// Turn req.body into an array of values
 		var colValues = Object.keys(transactionTableParameters).map(function (key) {
 			return transactionTableParameters[key];
@@ -129,18 +106,104 @@ export default class TransactionsController {
 				transactionQuery, colValues
 			)
 
-			// TODO Update Tags
+			if (tags) {
+				await client.query(
+					format("DELETE FROM Tags WHERE transaction_id = %L", transactionId)
+				);
+
+				for (const tag of tags) {
+					await client.query( // TODO: Do within single transaction
+						`
+							INSERT INTO Tags(
+								tag,
+								transaction_id
+							) VALUES ($1,$2);
+						`,
+						[tag, transactionId]
+					);
+				}
+			}
+
+			// Handle transfer transaction
+			// TODO test
+
+			const transactionCategory = await this.getTransactionType(client, transactionId);
+
+			if (transactionCategory === "TRANSFER") {
+				client.query(format(
+					`
+					UPDATE Transactions
+					SET value = %L
+					WHERE sister_transfer_transaction = %L
+					`,
+					-transactionTableParameters.value, // Negative needed for transfer
+					transactionId
+				)
+				)
+			}
+
+			const returnObject = req.body;
+			returnObject.tags = tags;
+			returnObject.transaction_id = transactionId;
+			res.send(req.body);
 
 			await client.query('COMMIT');
-
-			// TODO handle transfer queries
-
-			res.send(transactionQuery);
 		} catch (e) {
 			await client.query('ROLLBACK')
 			throw (e);
 		} finally {
 			client.release();
+		}
+	}
+	/**
+	 * 
+	 */
+	@Post("/transaction/transfer/:userId")
+	async createTransferTransaction(req, res) {
+
+	}
+
+	async getTransactionType(client: ClientBase, transactionId: number) {
+		const transactionCategory = (await client.query(format(
+			`
+				SELECT category_type
+				FROM Transactions, TransactionsCategories
+				WHERE category = category_id AND transaction_id = %L
+				`, transactionId
+		))).rows[0].category_type;
+
+		return transactionCategory;
+	}
+
+	async insertTransaction(client: ClientBase, t: Transaction) {
+		const result = await client.query(
+			`
+					INSERT INTO Transactions(
+						virtual_account,
+						physical_account,
+						value,
+						category,
+						timestampEpochSeconds,
+						description,
+						notes
+					) VALUES ($1,$2,$3,$4,$5,$6,$7)
+					RETURNING transaction_id;
+				`,
+			[t.virtual_account, t.physical_account, t.value, t.category, t.timestampEpochSeconds, t.description, t.notes]
+		);
+
+		t.transaction_id = result.rows[0].transaction_id;
+
+		for (const tag of t.tags) {
+			await client.query( // TODO: Do within single transaction
+				`
+						INSERT INTO Tags(
+							tag,
+							transaction_id
+						) VALUES ($1,$2);
+					`,
+				[tag, t.transaction_id]
+			);
 		}
 	}
 }
