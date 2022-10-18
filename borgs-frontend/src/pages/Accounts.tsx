f49dade02f9b61ede817eabfa1106c3d809a0d60
Alexander Shen <aes131@duke.edu>
2022-10-17 20:03:12 -0400

    added some stuff

--- borgs-frontend/src/pages/Accounts.tsx
+++ borgs-frontend/src/pages/Accounts.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { observer } from "mobx-react-lite";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
 
 export const Accounts = observer(() => {
 	return (
 		<div>
 			<h1>Accounts</h1>
+			<FormControl>
+  				<InputLabel htmlFor="my-input">Email address</InputLabel>
+  				<Input id="my-input" aria-describedby="my-helper-text" />
+			</FormControl>
 		</div>
+		
 	)
 });
