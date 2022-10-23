2632630eb3635ae85f18cc92ceee0ca2688fb875
Murilo Calegari de Souza <murilo478@gmail.com>
2022-10-23 12:47:13 -0400

    Added balance per account on Dashboard

--- borgs-frontend/src/stores/DashboardStore.ts
+++ borgs-frontend/src/stores/DashboardStore.ts
@@ -1,5 +1,10 @@
-import { makeAutoObservable } from "mobx"
+import { AxiosResponse } from "axios";
+import { action, makeAutoObservable, makeObservable, observable } from "mobx"
+import { axiosRequest } from "../api/api";
+import Transaction from "../model/Transaction";
+import Dashboard from "../model/Dashboard"
 import RootStore from "./RootStore";
+import UserStore from "./UserStore";
 
 export default class DashboardStore {
 	rootStore: RootStore;
@@ -7,5 +12,20 @@ export default class DashboardStore {
 	constructor(rootStore: RootStore) {
 		makeAutoObservable(this, { rootStore: false });
 		this.rootStore = rootStore;
+
+		// Update cache
+		this.updateBalance();
 	}
+
+	@observable currentBalanacesData : Dashboard[] = [];
+
+	@action
+    updateBalance() {
+		const {userStore} = this.rootStore
+		console.log(userStore.uid)
+		console.log('Update pa')
+        axiosRequest.get(`/balances/${userStore.uid}`)
+            .then(action((res) : AxiosResponse<Dashboard[], any> => this.currentBalanacesData = res.data));
+    }
 }
+
