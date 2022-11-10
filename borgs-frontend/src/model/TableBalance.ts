581f4053d8e1cc05f913eab35a147ae46127a0c3
Enzo Moraes Mescall <emm121@duke.edu>
2022-11-10 02:42:23 -0500

    added table balance object

---
+++ borgs-frontend/src/model/TableBalance.ts
@@ -0,0 +1,7 @@
+export default class TableBalance {
+	virtual_account!: number;
+	total_va_expenses!: number;
+	total_va_incomes!: number;
+	percent_total_expenses!: number;
+	percent_total_incomes!: number;
+}
