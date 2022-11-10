ca0c8a3d374f25c73ed6cfad98abee51451966a1
Martin Olarte <mo144@duke.edu>
2022-11-10 16:24:10 -0500

    Pertty

--- borgs-frontend/src/components/dashboard/accountCard.tsx
+++ borgs-frontend/src/components/dashboard/accountCard.tsx
@@ -1,10 +1,133 @@
 import * as React from 'react';
-import { observer } from 'mobx-react-lite';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Table } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 
-export const AccountCard = observer(() => {
-		return (
-			<div>
+const {Column, HeaderCell, Cell} = Table;
+	const BoldCell = ({rowData, dataKey, ...props }) => (
+		<Cell {...props}>
+		  <b>{rowData[dataKey]}</b>
+		</Cell>
+	  );
+	const CurrencyCell = ({rowData, dataKey, ...props }) => (
+		<Cell {...props}>
+		  $ {rowData[dataKey].toFixed(2)}
+		</Cell>
+	  );
+
+export default function accountCard(dashboardStore) {
+  	return (
+    <Card>
+      <CardContent>
+        <Typography sx={{ fontSize: 28 }} color="text.secondary" gutterBottom>
+          Account Balances
+        </Typography>
+
+		<div style={{
+		display: 'block', width: 500
+		}}>
+			<Table
+			autoHeight={true}
+			data={dashboardStore.currentBalancesData}
+			onRowClick={data => {
+				console.log(data);
+			}}
+			>	
+				<Column width={300} align="center" fixed>
+					<HeaderCell><b>Physical Account</b></HeaderCell>
+					<BoldCell dataKey="name" rowData={undefined} />
+				</Column>
+
+				<Column width={150} align="center">
+					<HeaderCell><b>Balance</b></HeaderCell>
+					<CurrencyCell dataKey="balance" rowData={undefined}/>
+				</Column>
+			</Table>	
+		</div>
+
+      </CardContent>
+      <CardActions>
+        <Button size="small">See Accounts</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export function categoryCard(dashboardStore) {
+  	return (
+    <Card>
+      <CardContent>
+        <Typography sx={{ fontSize: 28 }} color="text.secondary" gutterBottom>
+          Top Categories
+        </Typography>
+
+		<div style={{
+            display: 'block', width: 500
+        	}}>
+				<Table
+				autoHeight={true}
+				data={dashboardStore.currentTopCategories}
+				onRowClick={data => {
+					console.log(data);
+				}}
+				>
+					<Column width={300} align="center" fixed>
+						<HeaderCell><b>Category</b></HeaderCell>
+						<BoldCell dataKey="category" rowData={undefined} />
+					</Column>
+
+					<Column width={150} align="center">
+						<HeaderCell><b>Balance</b></HeaderCell>
+						<CurrencyCell dataKey="balance" rowData={undefined}/>
+					</Column>
+				</Table>	
 			</div>
-		);
-	}
-)
+      </CardContent>
+	  <CardActions>
+	  	<Button size="small">See Detailed Reports</Button>
+	  </CardActions>
+    </Card>
+  );
+}
+
+export function incomesAndExpensesCard(dashboardStore) {
+	return (
+  <Card>
+	<CardContent>
+	  <Typography sx={{ fontSize: 28 }} color="text.secondary" gutterBottom>
+		Incomes and Expenses Summary
+	  </Typography>
+
+	  <div style={{
+            display: 'block', width: 500
+        	}}>
+				<Table
+				autoHeight={true}
+				data={dashboardStore.currentExpensesIncomes}
+				onRowClick={data => {
+					console.log(data);
+				}}
+				>
+					<Column width={300} align="center" fixed>
+						<HeaderCell><b>Total Incomes</b></HeaderCell>
+						<CurrencyCell dataKey="total_incomes" rowData={undefined}/>
+					</Column>
+
+					<Column width={150} align="center">
+						<HeaderCell><b>Total Expenses</b></HeaderCell>
+						<CurrencyCell dataKey="total_expenses" rowData={undefined}/>
+					</Column>
+				</Table>	
+			</div>
+	</CardContent>
+	<CardActions>
+	  <Button size="small">See Detailed Reports</Button>
+	</CardActions>
+  </Card>
+);
+}
--- borgs-frontend/src/pages/MainPage.tsx
+++ borgs-frontend/src/pages/MainPage.tsx
@@ -5,109 +5,34 @@ import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
-import { ButtonGroup, List, Typography } from '@mui/material';
+import { Box, ButtonGroup, List, Typography } from '@mui/material';
 import DashboardStore from '../stores/DashboardStore';
 import Dashboard from '../model/Dashboard';
 import { useStores } from '../hooks/useStores';
-import { AccountCard } from '../components/dashboard/accountCard';
 import { Table } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
+import accountCard, { categoryCard, incomesAndExpensesCard } from '../components/dashboard/accountCard';
 
 export const MainPage = observer(() => {
 	const {dashboardStore} = useStores();
-	const {Column, HeaderCell, Cell} = Table;
 
 	return (
 		<div>
 			<h1>Home</h1>
-			<div>
 
-			<br></br>
-
-			<Typography>
-				Total balance = ${dashboardStore.currentTotalBalance.toFixed(2)}
+			<Typography variant='h4' align='center'>
+				Total Balance = <strong>$ {dashboardStore.currentTotalBalance.toFixed(2)}</strong>
 			</Typography>
 
-			<br></br>
-			<br></br>
+			<div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
 
-			<h2>Account Balances</h2>
+			{accountCard(dashboardStore)}
 
-			<div style={{
-            display: 'block', width: 500
-        	}}>
-				<Table
-				autoHeight={true}
-				data={dashboardStore.currentBalancesData}
-				onRowClick={data => {
-					console.log(data);
-				}}
-				>
-					<Column width={300} align="center" fixed>
-						<HeaderCell><b>Physical Account</b></HeaderCell>
-						<Cell dataKey="name" />
-					</Column>
+			{categoryCard(dashboardStore)}
 
-					<Column width={150} align="center">
-						<HeaderCell><b>Balance</b></HeaderCell>
-						<Cell dataKey="balance" />
-					</Column>
-				</Table>	
-			</div>
+			{incomesAndExpensesCard(dashboardStore)}
 
-			<br></br>
-			<br></br>
-
-			<h2>Top Categories</h2>
-
-			<div style={{
-            display: 'block', width: 500
-        	}}>
-				<Table
-				autoHeight={true}
-				data={dashboardStore.currentTopCategories}
-				onRowClick={data => {
-					console.log(data);
-				}}
-				>
-					<Column width={300} align="center" fixed>
-						<HeaderCell><b>Category</b></HeaderCell>
-						<Cell dataKey="category" />
-					</Column>
-
-					<Column width={150} align="center">
-						<HeaderCell><b>Balance</b></HeaderCell>
-						<Cell dataKey="balance" />
-					</Column>
-				</Table>	
-			</div>
-
-			<br></br>
-			<br></br>
-
-			<h2>Incomes and Expenses Summary</h2>
-
-			<div style={{
-            display: 'block', width: 500
-        	}}>
-				<Table
-				autoHeight={true}
-				data={dashboardStore.currentExpensesIncomes}
-				onRowClick={data => {
-					console.log(data);
-				}}
-				>
-					<Column width={300} align="center" fixed>
-						<HeaderCell><b>Total Incomes</b></HeaderCell>
-						<Cell dataKey="total_incomes" />
-					</Column>
-
-					<Column width={150} align="center">
-						<HeaderCell><b>Total Expenses</b></HeaderCell>
-						<Cell dataKey="total_expenses" />
-					</Column>
-				</Table>	
-			</div>
+			
 
 			</div>
 		</div>
