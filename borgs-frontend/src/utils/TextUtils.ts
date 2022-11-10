d6faf2039787cb925460c3a2e49f666d884a461c
Murilo Calegari <murilo.calegari@duke.edu>
2022-11-10 12:40:57 -0500

    Create util functions for currency text

---
+++ borgs-frontend/src/utils/TextUtils.ts
@@ -0,0 +1,11 @@
+import currency from "currency.js";
+
+export function formatCurrencyText(value) {
+    return currency(value,
+        {separator: ".", decimal: ",", symbol: "$"}
+    ).format();
+}
+
+export function computeCurrencyTextColor(value) {
+    return value < 0 ? "#c62828" : "#4caf50";
+}
