9d947a6aa66aff3b9631c8b84facfebbe4109a62
Martin Olarte <mo144@duke.edu>
2022-11-10 19:38:32 -0500

    Fixed few elements

--- borgs-frontend/src/pages/MainPage.tsx
+++ borgs-frontend/src/pages/MainPage.tsx
@@ -14,11 +14,16 @@ import 'rsuite/dist/rsuite.min.css';
 import accountCard, { categoryCard, incomesAndExpensesCard } from '../components/dashboard/accountCard';
 
 export const MainPage = observer(() => {
-	const {dashboardStore} = useStores();
+	const {dashboardStore, userStore} = useStores();
 
 	return (
 		<div>
-			<h1>Home</h1>
+			<Typography variant='h5' paddingLeft='3%' paddingTop='3%'>
+				Welcome User {userStore.uid}!
+			</Typography>
+			<Typography variant='h2' align='center' padding='1%'>
+				Home
+			</Typography>
 
 			<Typography variant='h4' align='center'>
 				Total Balance = <strong>$ {dashboardStore.currentTotalBalance.toFixed(2)}</strong>
